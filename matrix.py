dde2a3c9c89656e278991d47b36d4d5eaa0e49c3
PasHyde <79587588+PasHyde@users.noreply.github.com>
2021-10-13 14:27:32 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,87 +1,93 @@
 
 import math
-from relate import metrics, data_file
 import pandas as pd
-
+from relate import metrics
 
 '''
 Functions return different symmetric matrices (similarity and distance) using different calculations:
-string similarity metrics with (or without) standardizing function.
+string similarity metrics with or without standardizing function.
 '''
-# Call string similarity metrics (jaccard,sørensen_dice or overlap) from metrics module 
+# Call string similarity metrics from the metrics module
 metrics = metrics.select
-names = data_file.names
+# Default names of the analyzed strings
+names = ('text1', 'text2', 'text3', 'text4')
 
-def similarity_matrix(metrics, all_texts1, all_texts2):
+def similarity_matrix(metrics, texts1, texts2):
     '''
     Function returns a symmetric similarity matrix, which has the same number of rows and
     columns (n x n).
     
     Parameters:
-    metrics: 'jaccard' (jaccard_similarity_coefficient),
-             'sørensen_dice' (sørensen_dice coefficient),
-             'overlap' (overlap_coefficient),
-             'hamming' (hamming similarity)
-    all_texts1: first tuple (tokenized strings stored in the items of the tuple)
-    all_texts2: second tuple (tokenized strings stored in the items of the tuple)
+    metrics:
+    'jaccard' (Jaccard similarity coefficient),
+    'sorensen_dice' (Sorensen-Dice coefficient),
+    'overlap' (Overlap coefficient),
+    'levenshtein' (Levenshtein similarity),
+    'hamming' (Hamming similarity)
+    texts1: first tuple (strings stored in the items of the tuple)
+    texts2: second tuple (strings stored in the items of the tuple)
 
     Returns:
     matrix: n x n symmetric similarity matrix
     '''
    
-    # Compare a pair of tokenized strings at a time stored in tuples all_texts1 and all_texts2, return list and 
-    # sublists corresponding the number of strings in the dataset
-    SM = [[(metrics (a,b)) for a in all_texts1] for b in all_texts2]
+    # Compare a pair of strings at a time stored in tuples texts1 and texts2, return list of
+    # similarity values and sublists corresponding the number of strings in the dataset
+    SM = [[(metrics (a,b)) for a in texts1] for b in texts2]
     # Convert sublists into a matrix using pandas Dataframe
     matrix = pd.DataFrame(SM, columns=names, index=names)
     # show the whole matrix
     pd.set_option('display.width', None)
     return matrix
 
-def distance_matrix(metrics, all_texts1, all_texts2):
+def distance_matrix(metrics, texts1, texts2):
     '''
     Function returns a symmetric distance matrix, which has the same number of rows and
     columns (n x n).
 
     Parameters:
-    metrics: 'jaccard' (jaccard_similarity_coefficient),
-             'sørensen_dice' (sørensen_dice coefficient),
-             'overlap' (overlap_coefficient),
-             'hamming' (hamming similarity)
-    all_texts1: first tuple (tokenized strings stored in the items of the tuple)
-    all_texts2: second tuple (tokenized strings stored in the items of the tuple)
+    metrics:
+    'jaccard' (Jaccard similarity coefficient),
+    'sorensen_dice' (Sorensen-Dice coefficient),
+    'overlap' (Overlap coefficient),
+    'levenshtein' (Levenshtein similarity),
+    'hamming' (Hamming similarity)
+    texts1: first tuple (strings stored in the items of the tuple)
+    texts2: second tuple (strings stored in the items of the tuple)
 
     Returns:
     matrix: n x n symmetric distance matrix
     '''
-    # Compare a pair of tokenized strings at a time stored in tuples all_texts1 and all_texts2, return list of
+    # Compare a pair of strings at a time stored in tuples texts1 and texts2, return a list of
     # distance values ((1-metrics)*100) and sublists corresponding the number of strings in the dataset
-    DM = [[100-float(metrics (a,b)) for a in all_texts1] for b in all_texts2]
+    DM = [[100-float(metrics (a,b)) for a in texts1] for b in texts2]
     # Convert sublists into a matrix using pandas Dataframe
     matrix = pd.DataFrame(DM, columns=names, index=names)
     # show the whole matrix
     pd.set_option('display.width', None)
     return matrix
 
-def standardized_similarity_matrix(metrics, all_texts1, all_texts2):
+def standardized_similarity_matrix(metrics, texts1, texts2):
     '''
     Function returns a symmetric similarity matrix, which has the same number of rows
     and columns (n x n) with standardized values. The used standardizing function:
     original value - mean of the values in the dataset / standard deviation of the values in the dataset.
 
     Parameters:
-    metrics: 'jaccard' (jaccard_similarity_coefficient),
-             'sørensen_dice' (sørensen_dice coefficient),
-             'overlap' (overlap_coefficient),
-             'hamming' (hamming similarity)
-    all_texts1: first tuple (tokenized strings stored in the items of the tuple)
-    all_texts2: second tuple (tokenized strings stored in the items of the tuple)
+    metrics:
+    'jaccard' (Jaccard similarity coefficient),
+    'sorensen_dice' (Sorensen-Dice coefficient),
+    'overlap' (Overlap coefficient),
+    'levenshtein' (Levenshtein similarity),
+    'hamming' (Hamming similarity)
+    texts1: first tuple (strings stored in the items of the tuple)
+    texts2: second tuple (strings stored in the items of the tuple)
 
     Returns:
     matrix: n x n symmetric similarity matrix with standardized values
     '''
-    # Compare a pair of tokenized strings at a time stored in tuples all_texts1 and all_texts2, return list of similarity values
-    SM1 = [float(metrics (a,b)) for a in all_texts1 for b in all_texts2]
+    # Compare a pair of strings at a time stored in tuples texts1 and texts2, return list of similarity values
+    SM1 = [float(metrics (a,b)) for a in texts1 for b in texts2]
     # Ignore instances where the strings are compared to itself, resulting to value 100 (= 100 % similarity)
     SM  = [x for x in SM1 if x != 100]
     # Calculate mean of all the values (skipping value 100) in the dataset
@@ -96,34 +102,36 @@ def standardized_similarity_matrix(metrics, all_texts1, all_texts2):
     square_root = math.sqrt(divide)
     standard_deviation = square_root
     # Apply the standardization function to all values in the dataset, skip when the string is compared to itself
-    standardize = [0 if x == 100.0 else (x- mean)/standard_deviation for x in SM1]
+    standardize = [0.0 if x == 100.0 else (x- mean)/standard_deviation for x in SM1]
     # Return a list containing sublists corresponding the number of strings in the dataset
-    standardize = [standardize[x:x+len(all_texts1)] for x in range(0, len(standardize), len(all_texts1))]
+    standardize = [standardize[x:x+len(texts1)] for x in range(0, len(standardize), len(texts1))]
     # Convert sublists into a matrix using pandas Dataframe
     matrix = pd.DataFrame(standardize, columns=names, index=names)
     # show the whole matrix
     pd.set_option('display.width', None)
     return matrix
 
-def standardized_distance_matrix(metrics, all_texts1, all_texts2):
+def standardized_distance_matrix(metrics, texts1, texts2):
     '''
     Function returns a symmetric distance matrix, which has the same number of rows
     and columns (n x n) with standardized values. The used standardizing function:
     original value - mean of the values in the dataset / standard deviation of the values in the dataset.
 
     Parameters:
-    metrics: 'jaccard' (jaccard_similarity_coefficient),
-             'sørensen_dice' (sørensen_dice coefficient),
-             'overlap' (overlap_coefficient),
-             'hamming' (hamming similarity)
-    all_texts1: first tuple (tokenized strings stored in the items of the tuple)
-    all_texts2: second tuple (tokenized strings stored in the items of the tuple)
-
+    metrics:
+    'jaccard' (Jaccard similarity coefficient),
+    'sorensen_dice' (Sorensen-Dice coefficient),
+    'overlap' (Overlap coefficient),
+    'levenshtein' (Levenshtein similarity),
+    'hamming' (Hamming similarity)
+    texts1: first tuple (strings stored in the items of the tuple)
+    texts2: second tuple (strings stored in the items of the tuple)
+    
     Returns:
     matrix: n x n symmetric distance matrix with standardized values
     '''
-    # Compare a pair of tokenized strings at a time stored in tuples all_texts1 and all_texts2, return list of distance values ((1-metrics)*100)
-    DM1 = [100-float(metrics (a,b)) for a in all_texts1 for b in all_texts2]
+    # Compare a pair of strings at a time stored in tuples texts1 and texts2, return list of distance values ((1-metrics)*100)
+    DM1 = [100-(float(metrics (a,b))) for a in texts1 for b in texts2]
     # Ignore instances where the strings are compared to itself, resulting to value 0 (= 0 % distance)
     DM  = [x for x in DM1 if x != 0]
     # Calculate mean of all the values (skipping instances of 0) in the dataset
@@ -138,9 +146,9 @@ def standardized_distance_matrix(metrics, all_texts1, all_texts2):
     square_root = math.sqrt(divide)
     standard_deviation = square_root
     # Apply the standardization function to all values in the dataset, skip when the string is compared to itself
-    standardize = [0 if x == 0.0 else (x- mean)/standard_deviation for x in DM1]
+    standardize = [0.0 if x == 0.0 else (x- mean)/standard_deviation for x in DM1]
     # Return a list containing sublists corresponding the number of strings in the dataset
-    standardize = [standardize[x:x+len(all_texts1)] for x in range(0, len(standardize), len(all_texts1))]
+    standardize = [standardize[x:x+len(texts1)] for x in range(0, len(standardize), len(texts1))]
     # Convert sublists into a matrix using pandas Dataframe
     matrix = pd.DataFrame(standardize, columns=names, index=names)
     # show the whole matrix
@@ -148,7 +156,7 @@ def standardized_distance_matrix(metrics, all_texts1, all_texts2):
     return matrix
 
 # Use dictionary to call the functions
-select= {
+select={
     'similarity': similarity_matrix,
     'distance': distance_matrix,
     'st_similarity': standardized_similarity_matrix,
